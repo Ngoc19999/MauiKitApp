bb9bb9340d4af5b189fb827c978809d0c23ae1bb
goStay1 <ceo@nhadat24h.net>
2025-06-04 16:43:19 +0700

    SAsÁ

---
+++ MauiKit/Platforms/Android/CustomInfoWindowClickListener.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiMaps;
+
+using Android.Gms.Maps;
+
+internal class CustomInfoWindowClickListener(CustomMapHandler mapHandler)
+	: Java.Lang.Object, GoogleMap.IOnInfoWindowClickListener
+{
+	public void OnInfoWindowClick(Android.Gms.Maps.Model.Marker marker)
+	{
+		var pin = mapHandler.Markers.FirstOrDefault(x => x.marker.Id == marker.Id);
+		pin.pin?.SendInfoWindowClick();
+	}
+}
---
+++ MauiKit/Platforms/Android/MapCallbackHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiMaps;
+
+using Android.Gms.Maps;
+using Microsoft.Maui.Maps;
+
+class MapCallbackHandler(CustomMapHandler mapHandler) : Java.Lang.Object, IOnMapReadyCallback
+{
+	public void OnMapReady(GoogleMap googleMap)
+	{
+		mapHandler.UpdateValue(nameof(IMap.Pins));
+		mapHandler.Map?.SetOnMarkerClickListener(new CustomMarkerClickListener(mapHandler));
+		mapHandler.Map?.SetOnInfoWindowClickListener(new CustomInfoWindowClickListener(mapHandler));
+	}
+}
